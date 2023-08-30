db11ab128942f38b6261d7dafa07164c52e64a6d
iArctix <connortodd.ct@gmail.com>
2023-08-30 13:32:52 +0100

    Balls
    asdasd

--- DaleProject/Assets/Scripts/PlayerMovement.cs
+++ DaleProject/Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,11 @@ public class PlayerMovement : MonoBehaviour
     private bool isGrounded;
     public float fallMultiplier;
     public float lowjumpMultiplier;
+    private SpriteRenderer spriteRenderer;
+
+    //Pickaxe and Sprite Rotation
+    public Transform playerCenter; // Empty GameObject at the center of the player
+    public GameObject pickaxe; // The pickaxe GameObject
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -53,5 +58,30 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity += Vector2.up * Physics2D.gravity.y * (lowjumpMultiplier - 1) * Time.deltaTime;
         }
 
+    }
+
+    void pickaxerotation()
+    {
+        {
+            // Calculate the angle between playerCenter and mouse position
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 direction = mousePos - playerCenter.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            // Rotate the playerCenter (and the attached pickaxe)
+            playerCenter.rotation = Quaternion.Euler(0f, 0f, angle);
+
+            if (mousePos.x < playerCenter.position.x)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+                pickaxe.GetComponent<SpriteRenderer>().flipY = true;
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+                pickaxe.GetComponent<SpriteRenderer>().flipY = false;
+            }
+
+        }
     }
 }
