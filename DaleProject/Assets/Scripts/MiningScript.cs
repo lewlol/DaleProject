d441d601fa14e79ca74e3a10982fe5021e048dcc
iArctix <connortodd.ct@gmail.com>
2023-08-30 14:14:10 +0100

    sdf
    dsf

--- DaleProject/Assets/Scripts/MiningScript.cs
+++ DaleProject/Assets/Scripts/MiningScript.cs
@@ -5,6 +5,19 @@ public class MiningScript : MonoBehaviour
     public LayerMask ignoreRaycast;
     public float miningrange = 2f;
 
+    //Holding variables
+    private bool isHolding = false;
+    private float currentHoldTime = 0.0f;
+    public float requiredHoldTime = 2.0f; 
+
+
+
+    private Camera cam;
+
+    private void Start()
+    {
+        cam= Camera.main;
+    }
     private void Update()
     {
         miningblock();
@@ -14,14 +27,35 @@ public class MiningScript : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, float.MaxValue, ~ignoreRaycast);
+            RaycastHit2D hit = Physics2D.Raycast(cam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, float.MaxValue, ~ignoreRaycast);
 
             if (hit.collider != null && hit.collider.CompareTag("Block"))
             {
                 if (IsInRange(hit.collider.gameObject))
                 {
-                    BlockBreak(hit.collider.gameObject);
+                    isHolding = true;
                 }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (isHolding)
+            {
+                currentHoldTime = 0.0f;
+                isHolding = false;
+            }
+        }
+
+        if (isHolding)
+        {
+            currentHoldTime += Time.deltaTime;
+
+            if (currentHoldTime >= requiredHoldTime)
+            {
+                BlockBreak(hit.collider.gameObject);
+                currentHoldTime = 0.0f;
+                isHolding = false;
             }
         }
     }
