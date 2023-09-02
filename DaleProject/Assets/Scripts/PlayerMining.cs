4ed802188c9071333d964d5eae10dcb91ab55384
lewlol <lewdevbusiness@gmail.com>
2023-09-02 21:52:35 +0100

    PlayerMiningL

--- DaleProject/Assets/Scripts/PlayerMining.cs
+++ DaleProject/Assets/Scripts/PlayerMining.cs
@@ -4,5 +4,40 @@ using UnityEngine;
 
 public class PlayerMining : MonoBehaviour
 {
+    private void OnMouseEnter()
+    {
+        HighlightBlock();
+    }
 
+    private void OnMouseExit()
+    {
+        DeHighlightBlock();
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            BreakingBlock();
+        }
+    }
+    void BreakingBlock()
+    {
+
+    }
+
+    void BreakBlock()
+    {
+
+    }
+
+    void HighlightBlock()
+    {
+
+    }
+
+    void DeHighlightBlock()
+    {
+
+    }
 }
