656f8ed517958f6a390cad3df3fb7b4fe353ad50
iArctix <connortodd.ct@gmail.com>
2023-09-03 20:00:03 +0100

    Mining Pog?
    asdasd

--- DaleProject/Assets/Scripts/MiningScript.cs
+++ DaleProject/Assets/Scripts/MiningScript.cs
@@ -4,10 +4,15 @@ public class MiningScript : MonoBehaviour
 {
     public float miningRange = 2.0f;
     public float miningTime = 1.0f;
+    public float shakeMagnitude = 0.05f; // Adjust this as needed
 
     private bool isMining = false;
     private float miningTimer = 0.0f;
     private GameObject currentBlock;
+    private Vector3 originalBlockPosition;
+    private Transform childTransform; // The child transform to shake
+    private GameObject lastHighlightedBlock;
+    private GameObject previousHighlightedBlock;
 
     int stamina;
     public PlayerStatsData playerStats;
@@ -18,19 +23,30 @@ public class MiningScript : MonoBehaviour
     {
         RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 
+        if (lastHighlightedBlock != null && lastHighlightedBlock != hit.collider?.gameObject)
+        {
+            Unhighlightblock();
+        }
+        if (lastHighlightedBlock != null && !IsInRange(lastHighlightedBlock))
+        {
+            Unhighlightblock();
+        }
         if (currentBlock != null && (hit.collider == null || hit.collider.gameObject != currentBlock))
         {
             // Reset mining if the cursor moves away from the current block or a new block is highlighted
             isMining = false;
-            currentBlock = null;
             miningTimer = 0f;
+            ResetBlockPosition();
         }
 
         if (hit.collider != null && hit.collider.CompareTag("Block") && IsInRange(hit.collider.gameObject))
         {
+            HightlightBlock(hit.collider.gameObject);
             if (Input.GetMouseButtonDown(0))
             {
                 currentBlock = hit.collider.gameObject;
+                childTransform = currentBlock.transform.GetChild(0); // Assuming the child is at index 0
+                originalBlockPosition = childTransform.position;
                 isMining = true;
                 miningTimer = 0.0f;
             }
@@ -39,6 +55,7 @@ public class MiningScript : MonoBehaviour
             {
                 isMining = false;
                 miningTimer = 0f;
+                ResetBlockPosition();
             }
 
             if (isMining)
@@ -50,6 +67,10 @@ public class MiningScript : MonoBehaviour
                     BlockBreak(currentBlock);
                     isMining = false;
                     currentBlock = null; // Reset the current block
+                }
+                else
+                {
+                    ApplyShake(childTransform);
                 }
             }
         }
@@ -78,6 +99,28 @@ public class MiningScript : MonoBehaviour
         Destroy(block);
     }
 
+    private void ApplyShake(Transform transformToShake)
+    {
+        if (transformToShake != null)
+        {
+            Vector3 shakeOffset = new Vector3(
+                Random.Range(-shakeMagnitude, shakeMagnitude),
+                Random.Range(-shakeMagnitude, shakeMagnitude),
+                0
+            );
+
+            transformToShake.position = originalBlockPosition + shakeOffset;
+        }
+    }
+
+    private void ResetBlockPosition()
+    {
+        if (childTransform != null)
+        {
+            childTransform.position = originalBlockPosition;
+        }
+    }
+
     public void MinusStamina(int amount)
     {
         stamina -= amount;
@@ -90,5 +133,30 @@ public class MiningScript : MonoBehaviour
     public void RegenStamina()
     {
         stamina = playerStats.maxstamina;
+    }
+
+    private void HightlightBlock(GameObject block)
+    {
+        if(block != previousHighlightedBlock)
+        {
+            Unhighlightblock();
+            lastHighlightedBlock = block;
+            // Get the child object and enable it
+            GameObject childObject = block.transform.GetChild(0).gameObject;
+            SpriteRenderer childSpriteRenderer = childObject.GetComponent<SpriteRenderer>();
+            childSpriteRenderer.enabled = true;
+            previousHighlightedBlock = block;
+        }
+    }
+    private void Unhighlightblock()
+    {
+        if (lastHighlightedBlock != null)
+        {
+            GameObject childObject = lastHighlightedBlock.transform.GetChild(0).gameObject;
+            SpriteRenderer childSpriteRenderer = childObject.GetComponent<SpriteRenderer>();
+            childSpriteRenderer.enabled = false;
+            lastHighlightedBlock = null;
+            previousHighlightedBlock = null;
+        }
     }
 }
